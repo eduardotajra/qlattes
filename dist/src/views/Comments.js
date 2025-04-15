eeeba14749cd110e1fd74112bcffa346ca0ea87b
Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>
2025-04-15 19:53:14 -0300

    Eu nos créditos!
    Me adicionei nos créditos e fiz algumas alterações visuais. Além de reajustar a aba de comentários.

--- dist/src/views/Comments.js
+++ dist/src/views/Comments.js
@@ -1,27 +1,36 @@
-import {
-  Container,
-  Row,
-} from "reactstrap";
+import { Container, Row } from "reactstrap";
 
 const Comments = () => {
   return (
     <Container className="mt-4" fluid>
       <Row>
         <div className="col">
-          <h1>Comentários e Sugestões</h1>
-          <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLScWkosurzk1ukkTV28Yv9dGzIcw4jlmY6zWfCf9CjbEHU3Fig/viewform?embedded=true"
-            title="Formulário para comentários e sugestões"
-            width="100%"
-            height="535vh"
-            frameborder="none"
-          >
-            Carregando…
-          </iframe>
+          <h1>Comentários e sugestões</h1>
+          <p style={{ marginBottom: "1rem" }}>
+            Este espaço destina-se ao envio de sugestões, observações ou apontamentos relacionados ao uso e funcionamento deste produto. 
+            As contribuições aqui registradas são de grande relevância para o aprimoramento contínuo da ferramenta.
+          </p>
+          <div style={{ position: "relative", paddingBottom: "65%", height: 0, overflow: "hidden" }}>
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLScWkosurzk1ukkTV28Yv9dGzIcw4jlmY6zWfCf9CjbEHU3Fig/viewform?embedded=true"
+              title="Formulário de comentários e sugestões"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                border: "none",
+                borderRadius: "8px",
+              }}
+            >
+              Carregando…
+            </iframe>
+          </div>
         </div>
       </Row>
     </Container>
   );
-}
+};
 
 export default Comments;
--- dist/src/views/Credits.js
+++ dist/src/views/Credits.js
@@ -9,16 +9,25 @@ const Credits = () => {
       <Row>
         <div className="col">
           <h1>Créditos</h1>
-          <p>
-            Concepção e implementação:
-            <a href="https://sites.google.com/site/nabormendonca/" target="_blank"  rel="noreferrer">Nabor Mendonça</a>
-            (UNIFOR)
+          
+          <p><strong>Concepção e implementação:</strong><br />
+            <a href="https://sites.google.com/site/nabormendonca/" target="_blank" rel="noreferrer">
+              Nabor Mendonça
+            </a> (UNIFOR)
           </p>
-          <p>Consultoria de conteúdo: Andréia Formico (UNIFOR)</p>
-          <p>Consultoria técnica: Lucas Mendonça (Instituto Eldorado)</p>
-          <p>Consultoria sobre a fonte de dados da CAPES: André Luiz F. Batista (IFTM-MG)</p>
-          <p>Criação da fonte de dados da PUC-RS: Olimar Borges (PUC-RS)</p>
-          <p>Logo: variação sobre os logos do Qualis e da Plataforma Lattes</p>
+
+          <p><strong>Consultoria de conteúdo:</strong><br />Andréia Formico (UNIFOR)</p>
+
+          <p><strong>Consultoria técnica:</strong><br />Lucas Mendonça (Instituto Eldorado)</p>
+
+          <p><strong>Consultoria sobre a fonte de dados da CAPES:</strong><br />André Luiz F. Batista (IFTM-MG)</p>
+
+          <p><strong>Criação da fonte de dados da PUC-RS:</strong><br />Olimar Borges (PUC-RS)</p>
+
+          <p><strong>Colaboração no desenvolvimento:</strong><br />Eduardo de Morais Tajra (UNIFOR)</p>
+
+          <p><strong>Logo:</strong><br />Variação sobre os logos do Qualis e da Plataforma Lattes</p>
+
         </div>
       </Row>
     </Container>
